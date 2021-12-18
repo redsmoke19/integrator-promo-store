550036029a330d75b84cd451e87a526285df035d
Oleg Tsyganov <redsmoke19@gmail.com>
2021-12-18 16:10:40 +0400

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -11,10 +11,18 @@ const serve = require('./gulp/tasks/serve');
 const spritePNG = require('./gulp/tasks/spritePNG');
 const webP = require('./gulp/tasks/webP');
 
-const dev = gulp.parallel(pug2html, script, vendors, styles, imageMinify, webP, spriteSVG, spritePNG, fonts);
-
-exports.default = gulp.series(
-  clean,
-  dev,
-  serve
+const dev = gulp.parallel(
+  pug2html,
+  script,
+  vendors,
+  styles,
+  imageMinify,
+  // webP,
+  spriteSVG,
+  // spritePNG,
+  fonts,
 );
+
+exports.default = argv.prod
+  ? gulp.series(clean, dev)
+  : gulp.series(clean, dev, serve);
